1a2641de7d04147295db7e3f87b90c13e18bc9fa
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-09 03:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function fibonacci(num) {
 // your code here
+
+	let sum = 0;
+	let first = 0;
+	let second = 1;
+
+	if(num==1)
+	{
+		return 0;
+	}
+	else{
+		for(let i=2;i<=num; i++)
+			{
+				sum = first+second;
+				first = second;
+				second = sum;	
+			}
+		return second;
+	}
 }
 
-module.exports = fibonacci;
+module.exports = fibonacci(5);
